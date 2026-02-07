27b65b2c5e309aee4d4898c3fa21ccbac41aa7b6
goyour123 <goyour123@gmail.com>
2026-02-07 14:44:54 +0800

    Rewrite ipmitool_wrapper with argparse

--- script/python/ipmitool_wrapper/machine_1.py
+++ script/python/ipmitool_wrapper/machine_1.py
@@ -2,16 +2,21 @@
 
 import subprocess
 import json
-import os, sys
+import os
+import argparse
 
 if __name__ == '__main__':
     with open('machines.json', 'r') as j:
         config = json.load(j)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ipmi_args', type=str, nargs='+')
+    args = parser.parse_args()
+
     script_name = os.path.splitext(os.path.basename(__file__))[0]
 
     ipmi_cmd = ['ipmitool', '-I', 'lanplus', '-H', config[script_name], '-U', 'admin', '-P', 'admin']
-    ipmi_cmd.extend(sys.argv[1:])
+    ipmi_cmd.extend(args.ipmi_args)
 
     result = subprocess.run(ipmi_cmd, capture_output=True, text=True)
     print (result.stdout)
